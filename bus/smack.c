5dc60c3a474620712ea46ece5f7abe04ef8af5cd
Sehong Na <sehong.na@samsung.com>
2014-05-31 12:35:29 +0900

    Initialize Tizen 2.3

---
+++ bus/smack.c
@@ -0,0 +1,206 @@
+/* smack.c - Provide interface to query smack context
+ *
+ * Author: Brian McGillion <brian.mcgillion@intel.com>
+ * Copyright © 2012 Intel Corporation
+ *
+ * Licensed under the Academic Free License version 2.1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+#include <config.h>
+#include "smack.h"
+
+#include <dbus/dbus-internals.h>
+
+#include "connection.h"
+#include "services.h"
+#include "utils.h"
+#include "policy.h"
+
+#include <stdlib.h>
+
+#ifdef HAVE_ERRNO_H
+#include <errno.h>
+#endif
+#ifdef DBUS_ENABLE_SMACK
+#include <sys/smack.h>
+#endif
+
+int
+have_smack(void)
+{
+#ifdef DBUS_ENABLE_SMACK
+  static int have = -1;
+
+  if (have == -1)
+    {
+      if(smack_smackfs_path() == NULL)
+        have = 0;
+      else
+        have = 1;
+    }
+  return have;
+#else
+  return 0;
+#endif
+}
+
+char *
+bus_smack_get_label (DBusConnection *connection, DBusError *error)
+{
+#ifdef DBUS_ENABLE_SMACK
+  char *label;
+  int sock_fd;
+
+  if (!have_smack())
+    return NULL;
+  if (!dbus_connection_get_socket(connection, &sock_fd))
+    {
+      dbus_set_error (error, DBUS_ERROR_FAILED,
+                      "Failed to get the socket descriptor of the connection.\n");
+      _dbus_verbose ("Failed to get socket descriptor of connection for Smack check.%s\n");
+      return NULL;
+    }
+
+  /* retrieve an ascii, null-terminated string that defines the Smack context of the connected socket */
+  if (smack_new_label_from_socket(sock_fd, &label) < 0)
+    {
+      dbus_set_error (error, DBUS_ERROR_FAILED,
+                      "Failed to read the Smack context from the connection: %s.\n",
+                      _dbus_strerror (errno));
+      _dbus_verbose ("Failed to read the Smack context from the connection: %s.\n",
+                     _dbus_strerror (errno));
+      return NULL;
+    }
+  return label;
+#else
+  return NULL;
+#endif
+}
+
+void
+bus_smack_label_free (char *label)
+{
+  if (label)
+    free (label);
+}
+
+#ifdef DBUS_ENABLE_SMACK
+static int
+bus_smack_has_access (const char *subject, const char *object,
+                      const char *access)
+{
+
+  if (!have_smack())
+    return TRUE;
+  return smack_have_access (subject, object, access);
+}
+#endif
+
+/**
+ * Calculate the list of rules that apply to a connection.
+ *
+ * @param connection The inbound conenction
+ * @param rules_by_smack_label The table of object labels -> rules mapping
+ * @param allowed_list the list of permitted rules if it exists, otherwise NULL.
+ * @returns TRUE on success, False otherwise.
+ */
+dbus_bool_t
+bus_smack_generate_allowed_list (DBusConnection *connection,
+                                 DBusHashTable  *rules_by_smack_label,
+                                 DBusList **allowed_list)
+{
+#ifdef DBUS_ENABLE_SMACK
+  const char *subject_label;
+  DBusHashIter iter;
+  int is_allowed;
+  DBusList *rule_list;
+
+  /* the label of the subject, is the label on the new connection,
+     either the service itself or one of its clients */
+  subject_label = bus_connection_get_smack_label (connection);
+  if (subject_label == NULL)
+    return NULL;
+
+  rule_list = dbus_new0 (DBusList*, 1);
+  if (rule_list == NULL)
+    goto nomem;
+
+  /* Iterate over all the smack labels we have parsed from the .conf files */
+  _dbus_hash_iter_init (rules_by_smack_label, &iter);
+  while (_dbus_hash_iter_next (&iter))
+    {
+      DBusList *link;
+      const char *object_label = _dbus_hash_iter_get_string_key (&iter);
+      /* the list here is all the rules that are 'protected'
+         by the SMACK label named $object_label */
+      DBusList **list = _dbus_hash_iter_get_value (&iter);
+
+      for (link = _dbus_list_get_first_link (list);
+           link != NULL;
+           link = _dbus_list_get_next_link (list, link))
+        {
+          BusPolicyRule *rule = link->data;
+          is_allowed = 0;
+
+          switch (rule->type)
+            {
+            case BUS_POLICY_RULE_OWN:
+              is_allowed = bus_smack_has_access (subject_label,
+                                                 object_label,
+                                                 "RWX");
+              break;
+            case BUS_POLICY_RULE_SEND:
+              is_allowed = bus_smack_has_access (subject_label,
+                                                 object_label,
+                                                 "W");
+              break;
+            case BUS_POLICY_RULE_RECEIVE:
+              is_allowed = bus_smack_has_access (subject_label,
+                                                 object_label,
+                                                 "R");
+              break;
+            default:
+              continue;
+            }
+
+          if ((is_allowed) || (is_allowed == -1)) /* access allowed or access isn't possible to check */
+            {
+              if (!_dbus_list_append (rule_list, rule))
+                goto nomem;
+
+              bus_policy_rule_ref (rule);
+            }
+
+          _dbus_verbose ("permission request subject (%s) -> object (%s) : %d", subject_label, object_label, is_allowed);
+        }
+    }
+
+  *allowed_list = rule_list;
+  return TRUE;
+
+nomem:
+  if (rule_list != NULL)
+    {
+      _dbus_list_clear (&rule_list);
+      dbus_free (rule_list);
+    }
+  return FALSE;
+#else
+  return TRUE;
+#endif
+}
---
+++ packaging/dbus.spec
@@ -0,0 +1,145 @@
+Name:		dbus
+Summary:	D-Bus message bus
+Version:	1.6.4
+Release:	4
+Group:		System/Libraries
+License:	GPLv2+ or AFL
+URL:		http://www.freedesktop.org/software/dbus/
+Source0:	http://dbus.freedesktop.org/releases/%{name}/%{name}-%{version}.tar.gz
+Source1:	dbus-daemon_run
+Source2:	dbus-user.socket
+%if "%{_repository}" == "wearable"
+Source3:	dbus-user.service
+%else
+Source3:        dbus-user-mobile.service
+%endif
+Source4:	system.conf
+Source1001:	dbus.manifest
+Requires:	%{name}-libs = %{version}
+BuildRequires:  expat-devel >= 1.95.5
+BuildRequires:  libtool
+#BuildRequires:  pkgconfig(x11)
+BuildRequires:  pkgconfig(libsmack)
+#BuildRequires:  pkgconfig(libsystemd-daemon)
+#BuildRequires:  pkgconfig(libsystemd-login)
+
+%description
+D-Bus is a system for sending messages between applications. It is used both
+for the systemwide message bus service, and as a per-user-login-session
+messaging facility.
+
+
+%package libs
+Summary:    Libraries for accessing D-Bus
+Group:      System/Libraries
+#FIXME: This is circular dependency
+Requires:   %{name} = %{version}-%{release}
+
+%description libs
+Lowlevel libraries for accessing D-Bus.
+
+%package devel
+Summary:    Libraries and headers for D-Bus
+Group:      Development/Libraries
+Requires:   %{name} = %{version}-%{release}
+Requires:   pkgconfig
+
+%description devel
+Headers and static libraries for D-Bus.
+
+%prep
+%setup -q -n %{name}-%{version}
+
+%build
+cp %{SOURCE1001} .
+#CFLAGS="$CFLAGS -DUSE_MONOTONIC"
+#LDFLAGS="$LDFLAGS -lrt"
+
+%reconfigure --disable-static \
+    --exec-prefix=/ \
+    --bindir=%{_bindir} \
+    --libexecdir=%{_libdir}/dbus-1 \
+    --sysconfdir=%{_sysconfdir} \
+    --libdir=%{_libdir} \
+    --disable-asserts \
+    --disable-xml-docs \
+    --disable-selinux \
+    --disable-libaudit \
+    --enable-tests=no \
+    --with-system-pid-file=%{_localstatedir}/run/messagebus.pid \
+    --with-dbus-user=root \
+    --with-systemdsystemunitdir=%{_libdir}/systemd/system \
+    --enable-smack \
+
+make %{?jobs:-j%jobs}
+
+%install
+%make_install
+%remove_docs
+
+mv %{buildroot}/etc/dbus-1/system.conf %{buildroot}/etc/dbus-1/system.conf.systemd
+install -m644 %{SOURCE4} %{buildroot}/etc/dbus-1/system.conf
+
+mkdir -p %{buildroot}%{_libdir}/pkgconfig
+# Change the arch-deps.h include directory to /usr/lib instead of /lib
+sed -e 's@-I${libdir}@-I${prefix}/%{_lib}@' %{buildroot}%{_libdir}/pkgconfig/dbus-1.pc
+
+mkdir -p %{buildroot}%{_datadir}/dbus-1/interfaces
+
+ln -s dbus.service %{buildroot}%{_libdir}/systemd/system/messagebus.service
+
+mkdir -p %{buildroot}%{_libdir}/systemd/user
+install -m0644 %{SOURCE2} %{buildroot}%{_libdir}/systemd/user/dbus.socket
+install -m0644 %{SOURCE3} %{buildroot}%{_libdir}/systemd/user/dbus.service
+
+mkdir -p $RPM_BUILD_ROOT%{_datadir}/license
+cat COPYING > $RPM_BUILD_ROOT%{_datadir}/license/dbus
+cat COPYING > $RPM_BUILD_ROOT%{_datadir}/license/dbus-libs
+
+%post
+mkdir -p /var/lib/dbus
+ln -sf %{_sysconfdir}/machine-id /var/lib/dbus/machine-id
+
+%post libs -p /sbin/ldconfig
+
+%postun libs -p /sbin/ldconfig
+
+%files
+%defattr(-,root,root,-)
+%{_datadir}/license/dbus
+%{_bindir}/dbus-cleanup-sockets
+%{_bindir}/dbus-daemon
+%{_bindir}/dbus-monitor
+%{_bindir}/dbus-send
+%{_bindir}/dbus-uuidgen
+%{_bindir}/dbus-launch
+%dir %{_sysconfdir}/dbus-1
+%config(noreplace) %{_sysconfdir}/dbus-1/session.conf
+%dir %{_sysconfdir}/dbus-1/session.d
+%config(noreplace) %{_sysconfdir}/dbus-1/system.conf
+%config(noreplace) %{_sysconfdir}/dbus-1/system.conf.systemd
+%dir %{_sysconfdir}/dbus-1/system.d
+%dir %{_libdir}/dbus-1
+%attr(4750,root,dbus) %{_libdir}/dbus-1/dbus-daemon-launch-helper
+%{_libdir}/systemd/system/*
+%{_libdir}/systemd/user/*
+%dir %{_datadir}/dbus-1
+%{_datadir}/dbus-1/interfaces
+%{_datadir}/dbus-1/services
+%{_datadir}/dbus-1/system-services
+%dir %{_localstatedir}/run/dbus
+%dir %{_localstatedir}/lib/dbus
+%manifest dbus.manifest
+
+%files libs
+%defattr(-,root,root,-)
+%{_datadir}/license/dbus-libs
+%{_libdir}/libdbus-1.so.3*
+
+%files devel
+%defattr(-,root,root,-)
+%{_libdir}/libdbus-1.so
+%{_includedir}/dbus-1.0/dbus/dbus*.h
+%dir %{_libdir}/dbus-1.0
+%{_libdir}/dbus-1.0/include/dbus/dbus-arch-deps.h
+%{_libdir}/pkgconfig/dbus-1.pc
